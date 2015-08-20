972824239636c20ef099eef90969ebfb14dcde63
Satoshi Shiraishi <sachoco@gmail.com>
2015-08-20 21:42:27 +0200

    Press page

--- functions.php
+++ functions.php
@@ -712,4 +712,12 @@ class iii_Widget_Recent_Posts extends WP_Widget {
 }
 
 add_action( 'widgets_init', create_function( '', 'register_widget( "iii_Widget_Recent_Posts" );' ) );
-	
+
+//Gets post cat slug and looks for single-[cat slug].php and applies it
+add_filter('single_template', create_function(
+	'$the_template',
+	'foreach( (array) get_the_category() as $cat ) {
+		if ( file_exists(TEMPLATEPATH . "/single-{$cat->slug}.php") )
+		return TEMPLATEPATH . "/single-{$cat->slug}.php"; }
+	return $the_template;' )
+);
