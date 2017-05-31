66236dc4892a318dbd2e27ffd6b580c835342021
Satoshi Shiraishi <sachoco@gmail.com>
2017-05-31 08:23:53 +0200

    research archive name change

--- functions.php
+++ functions.php
@@ -246,7 +246,7 @@ function custom_post_research() {
 			'menu_position' => 7, /* this is what order you want it to appear in on the left hand side menu */
 			// 'menu_icon' => 'dashicons-calendar', /* the icon for the custom post type menu */
 			'rewrite'	=> array( 'slug' => 'research', 'with_front' => false ), /* you can specify its url slug */
-			'has_archive' => 'researches', /* you can rename the slug here */
+			'has_archive' => 'research', /* you can rename the slug here */
 			'capability_type' => 'post',
 			'hierarchical' => false,
 			/* the next one is important, it tells what's enabled in the post editor */
