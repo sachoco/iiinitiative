2b01dcd00849c5991bf353fb98fb5a7a8fc2825e
Satoshi Shiraishi <sachoco@gmail.com>
2019-07-12 01:12:33 +0200

    Add Workshop post-type

--- functions.php
+++ functions.php
@@ -313,6 +313,53 @@ function custom_post_residency() {
 	add_action( 'init', 'custom_post_residency');
 /****************************************************************/
 
+function custom_post_workshop() {
+	// creating (registering) the custom type
+	register_post_type( 'workshop', /* (http://codex.wordpress.org/Function_Reference/register_post_type) */
+		// let's now add all the options for this post type
+		array( 'labels' => array(
+			'name' => __( 'Workshop', 'iii' ), /* This is the Title of the Group */
+			'singular_name' => __( 'Workshop', 'iii' ), /* This is the individual type */
+			'all_items' => __( 'All Workshop', 'iii' ), /* the all items menu item */
+			'add_new' => __( 'Add New', 'iii' ), /* The add new menu item */
+			'add_new_item' => __( 'Add New Workshop', 'iii' ), /* Add New Display Title */
+			'edit' => __( 'Edit', 'iii' ), /* Edit Dialog */
+			'edit_item' => __( 'Edit Workshop', 'iii' ), /* Edit Display Title */
+			'new_item' => __( 'New Workshop', 'iii' ), /* New Display Title */
+			'view_item' => __( 'View Workshop', 'iii' ), /* View Display Title */
+			'search_items' => __( 'Search Workshop', 'iii' ), /* Search Custom Type Title */
+			'not_found' =>  __( 'Nothing found in the Database.', 'iii' ), /* This displays if there are no entries yet */
+			'not_found_in_trash' => __( 'Nothing found in Trash', 'iii' ), /* This displays if there is nothing in the trash */
+			'parent_item_colon' => ''
+			), /* end of arrays */
+			'description' => __( 'Workshop', 'iii' ), /* Custom Type Description */
+			'public' => true,
+			'publicly_queryable' => true,
+			'exclude_from_search' => false,
+			'show_ui' => true,
+			'query_var' => true,
+			'menu_position' => 7, /* this is what order you want it to appear in on the left hand side menu */
+			// 'menu_icon' => 'dashicons-calendar', /* the icon for the custom post type menu */
+			'rewrite'	=> array( 'slug' => 'workshop', 'with_front' => false ), /* you can specify its url slug */
+			'has_archive' => 'workshop', /* you can rename the slug here */
+			'capability_type' => 'post',
+			'hierarchical' => false,
+			/* the next one is important, it tells what's enabled in the post editor */
+			'supports' => array( 'title', 'editor', 'author', 'thumbnail', 'excerpt', 'trackbacks', 'custom-fields', 'comments', 'revisions', 'sticky')
+		) /* end of options */
+	); /* end of register post type */
+
+	/* this adds your post categories to your custom post type */
+	// register_taxonomy_for_object_type( 'category', 'production' );
+	/* this adds your post tags to your custom post type */
+	// register_taxonomy_for_object_type( 'post_tag', 'production' );
+
+}
+
+	// adding the function to the Wordpress init
+	add_action( 'init', 'custom_post_workshop');
+
+
 // custom post types : artist, work, news
 
 function create_post_type() {
