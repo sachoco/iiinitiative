915a498d0f33f0ac87d6d59da0329411a9da4c76
Satoshi Shiraishi <sachoco@gmail.com>
2020-05-02 17:09:02 +0200

    Upcoming workshops

--- archive-workshop.php
+++ archive-workshop.php
@@ -31,6 +31,85 @@
           // echo $description[0]->post_content;
           endif;
            ?>
+           <?php
+              $args = array(
+                  'post_type' => 'event',
+                  'post_state' => 'publish',
+                  'meta_key' => 'date_from',
+                  'orderby' => 'meta_value_num',
+                  'order' => 'ASC',
+                  'posts_per_page' => -1,
+                  'meta_query' => array(
+                    'relation' => 'AND',
+                      array(
+                          'key' => 'date_from',
+                          'value' => date("Ymd", strtotime("now")),
+                          'type' => 'NUMERIC',
+                          'compare' => '>'
+                      ),
+                      array(
+                          'key' => 'is_workshop',
+                          'value' => '1',
+                          'compare' => '=='
+                      )
+                  )
+              );
+
+              $the_query = new WP_Query( $args );
+          ?>
+  <?php if ($the_query->have_posts()) : ?>
+           <div class="related_events">
+           <div class="event-list ">
+               <h3 class="">Upcoming Workshops</h3>
+               <ul class="">
+       <?php while ($the_query->have_posts()) : $the_query->the_post(); if(!$display_item) $display_item = get_the_ID(); ?>
+               <li>
+                   <a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
+       <?php echo '<time>';
+
+
+
+               if(get_field('date_from')){
+                       $unixtimestamp = strtotime(get_field('date_from'));
+                       $date_from = date_i18n("d M, Y", $unixtimestamp);
+                       echo $date_from;
+                   if(get_field('date_until')){
+                       $unixtimestamp = strtotime(get_field('date_until'));
+                       $date_until = date_i18n("d M, Y", $unixtimestamp);
+                       echo " - ". $date_until;
+                   }
+               }else{
+                   //$event_start_date = rwmb_meta( 'event_start_date');
+                   //if('' != $event_start_date){
+                       the_date();
+                   //}
+                   $event_end_date = rwmb_meta( 'event_end_date');
+                   if('' != $event_end_date){
+                       echo ' - '.$event_end_date;
+                   }
+               }
+
+               $locations = get_field('location');
+               if($locations) {
+                   foreach($locations as $location){
+                       echo ", ".$location[location]; //''
+                   }
+               }else{
+                   $locations = rwmb_meta( 'event_location');
+                   foreach($locations as $location){
+                       echo ", ".$location; //''
+                   }
+               }
+               echo '</time>';
+       ?>
+               </li>
+       <?php endwhile; ?>
+               </ul>
+           </div>
+         </div>
+         <div class="clear">
+     <?php endif; ?>
+
         <!-- <div class="sort">
             <p>
               Filter:
